a1ab3581e578dca7dfe8a9da576f37fbee068ef7
Haniem <haniemcs@gmail.com>
2024-03-29 10:51:02 +0500

    Login function in AuthController

--- backend/app/Http/Controllers/Api/Auth/AuthController.php
+++ backend/app/Http/Controllers/Api/Auth/AuthController.php
@@ -10,18 +10,56 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
 
+        if($validator->fails()){
+            return response()->json([
+                'message' => 'Validations fails',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::where('email', $request->email)->first();
+
+
+        if($user){
+
+            if(Hash::check($request->password, $user->password)){
+
+
+                $token = $user->createToken('auto-token')->plainTextToken;
+
+                return response()->json([
+                    'message' => 'Incorrect Successfull',
+                    'token' => $token,
+                    'data' => $user
+                ], 200);
+
+            }else {
+                return response()->json([
+                    'message' => 'Incorrect credentials'
+                ], 400);
+            }
+        } else {
+            return response()->json([
+                'message' => 'Incorrect credentials'
+            ], 400);
+
+        }
     }
 
-    public function register(Request $request)
+public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'name'=>'required|min:2|max:100',
             'surname'=>'required|min:2|max:100',
             'lastname'=>'max:100',
             'username'=>'alpha:ascii|required|unique:users',
-            'email'=>'required|unique:users|',
+            'email'=>'required|unique:users|email',
             'specialization_id'=>'required',
             'position_id'=>'required',
             'password'=>'bail|required',
@@ -48,7 +86,7 @@ class AuthController extends Controller
         ]);
 
         return response()->json([
-            'message' => 'Validations fails',
+            'message' => 'Validations succesfull',
             'data' => $user
         ], 200);
 
