3d137cd698580c92483d6c68593915d7be55e09e
Benik800 <sasha.abdulfaizov@mail.ru>
2024-04-06 22:21:30 +0500

    50_add controllers getPosts and createPost, change Models/Post

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Post extends Model
 {
-    use HasFactory;
-}
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+
+    protected $fillable =
+     [
+        "post_title",
+        "post_description",
+        "post_text",
+        "user_id",
+        "type_id",
+        "state"
+     ];
+};
+
+
+
