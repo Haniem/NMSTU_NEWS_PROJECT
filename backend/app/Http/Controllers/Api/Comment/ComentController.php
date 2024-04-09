5e69bcc80d89915c35809257671e74dbbc9c5251
Тимур Каримов <karimovt354@gmail.com>
2024-04-09 21:01:28 +0500

    60 get comments function

---
+++ backend/app/Http/Controllers/Api/Comment/ComentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Comment;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComentController extends Controller
+{
+    //
+}
